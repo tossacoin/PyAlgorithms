d46f0bdc534cb41a684b8d91fa7aac85b89cef21
tossacoin <46962504+tossacoin@users.noreply.github.com>
2019-06-05 11:21:42 +0300

    Update les_2_task_8.py

--- les_2_task_8.py
+++ les_2_task_8.py
@@ -8,7 +8,7 @@ def count_digit(curnum, dig):
     k=0;
 #    curnum = num;
 #    i = 1;
-    if curnum ==0 and dig = 0:
+    if curnum ==0 and dig == 0:
         k=1;
     while curnum!=0:
         temp = round(curnum % (10));
